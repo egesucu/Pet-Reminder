64ecfe15d9777629bbfac3addc05d261c0346ec7
Ege Sucu <egesucu@icloud.com>
2023-07-09 22:12:22 +0300

    Added cloudkit method to see what's the status.

--- Pet Reminder/Utilities/PetReminderApp.swift
+++ Pet Reminder/Utilities/PetReminderApp.swift
@@ -8,19 +8,45 @@
 
 import SwiftUI
 import SwiftData
+import CloudKit
 
 @main
 struct PetReminderApp: App {
 
     @Environment(\.scenePhase) var scenePhase
     @AppStorage(Strings.tintColor) var tintColor = Color(uiColor: .systemGreen)
+    @State private var cloudEnabled = false
 
     var body: some Scene {
         WindowGroup {
             MainView()
                 .modelContainer(for: [Pet.self, Feed.self, Vaccine.self])
                 .tint(tintColor)
+                .onAppear(perform: checkIcloud)
         }
 
+    }
+
+    func checkIcloud() {
+        let container = CKContainer(identifier: "iCloud.com.softhion.Pet-Reminder")
+        container.accountStatus(completionHandler: { status, error in
+            if let error {
+                print(error.localizedDescription)
+            }
+            switch status {
+            case .couldNotDetermine:
+                print("couldNotDetermine Errors")
+            case .available:
+                print("available Errors")
+            case .restricted:
+                print("restricted Errors")
+            case .noAccount:
+                print("noAccount Errors")
+            case .temporarilyUnavailable:
+                print("temporarilyUnavailable Errors")
+            @unknown default:
+                print("Unknown CloudKit Errors")
+            }
+        })
     }
 }
