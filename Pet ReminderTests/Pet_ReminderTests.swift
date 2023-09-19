8093c95f5ffa50ebc63c497a55bca54cdda7a40a
Ege Sucu <egesucu@icloud.com>
2023-09-19 18:45:09 +0300

    Wrote an Unit test for Other notification removal

--- Pet ReminderTests/Pet_ReminderTests.swift
+++ Pet ReminderTests/Pet_ReminderTests.swift
@@ -7,6 +7,8 @@
 //
 
 import XCTest
+import CoreData
+@testable import Pet_Reminder
 
 // swiftlint: disable type_name
 final class Pet_ReminderTests: XCTestCase {
@@ -17,6 +19,31 @@ final class Pet_ReminderTests: XCTestCase {
         let stringKey: String.LocalizationValue = "notification_content \(name)"
         let localization = String(localized: stringKey)
         XCTAssertTrue(localization != notExpectedOutput)
+    }
+    
+    func testOtherNotificationDeletion() async throws {
+        let notificationManager = NotificationManager()
+        
+        let fetchRequest: NSFetchRequest<Pet> = Pet.fetchRequest()
+        let context = PersistenceController.preview.container.viewContext
+        let pets = try context.fetch(fetchRequest)
+        
+        for pet in pets {
+            await notificationManager.createNotifications(for: pet)
+        }
+        
+        await notificationManager.createNotification(of: "Bud", with: .morning, date: .now)
+        await notificationManager.createNotification(of: "Bud", with: .evening, date: .now)
+        await notificationManager.createNotification(of: "Bud", with: .birthday, date: .now)
+        
+        
+        await notificationManager.removeOtherNotifications(of: pets)
+        
+        async let notifications = notificationManager.notificationCenter.pendingNotificationRequests()
+        let identifiers = await notifications.map(\.identifier)
+        print("Test Notification Identifiers: ", identifiers)
+        
+        XCTAssertFalse(identifiers.contains("Bud"))
     }
 
 }
